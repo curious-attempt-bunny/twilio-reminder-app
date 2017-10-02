2d265cc1e88b86687a34f4ebbb39706020ec3262
Merlyn <curious.attempt.bunny@gmail.com>
2017-10-01 18:31:01 -0700

    Update home page title.

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -30,7 +30,7 @@ class App extends React.Component {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="/">dokku-rails-omniauth-bootstrap-react</a>
+          <a className="navbar-brand" href="/">twilio-reminder-app</a>
         </div>
         <div id="navbar" className="navbar-collapse collapse">
           { this.renderAuthButton() }
